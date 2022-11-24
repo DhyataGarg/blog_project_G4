920e6f4b74017b0e22145a41d48ded6f256935cd
DhyataGarg <dhyatagarg@gmail.com>
2022-11-24 14:54:27 +0530

    changes

--- src/blog/controllers.js
+++ src/blog/controllers.js
@@ -7,8 +7,21 @@ const createNewBlog = async (req, res) => {
 
 const getBlogs = async (req, res) => {
     var blogs = await Blog.find({ user_id: req.user }).populate("user_id");
-
     return res.json({ status: "Done", blogs });
 }
 
-module.exports = { createNewBlog, getBlogs };
+const updateBlog = async (req, res) => {
+    var { blog_id } = req.body;
+    await Blog.updateOne({ _id: blog_id }, { $set: req.body });
+
+    var blog = await Blog.findById(blog_id);
+    return res.json({ status: "Done", blog });
+}
+
+const deleteBlog = async (req, res) => {
+    await Blog.deleteOne({ _id: req.body.blog_id });
+
+    return res.json({ status: "Done" });
+}
+
+module.exports = { createNewBlog, getBlogs, updateBlog, deleteBlog };
